b9d8e8bb918961acbf0ab299c80335abe8f76efa
hui <125173679@qq.com>
2021-06-22 16:22:07 +0800

    feat: processing the request header

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { transformRequest } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 import { buildURL } from './helpers/url'
 import { AxiosRequestConfig } from './types/index'
 import xhr from './xhr'
@@ -11,6 +12,7 @@ function axios(config: AxiosRequestConfig): void {
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformUrl(config)
   config.data = transformRequestData(config)
+  config.headers = transformHeaders(config)
 }
 
 function transformUrl(config: AxiosRequestConfig): string {
@@ -20,6 +22,11 @@ function transformUrl(config: AxiosRequestConfig): string {
 
 function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
+}
+
+function transformHeaders(config: AxiosRequestConfig): any {
+  const { headers = {}, data } = config
+  return processHeaders(headers, data)
 }
 
 export default axios
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,11 +1,19 @@
 import { AxiosRequestConfig } from './types/index'
 
 export default function xhr(config: AxiosRequestConfig) {
-  const { data = null, url, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
 
   request.open(method.toUpperCase(), url!, true)
+
+  Object.keys(headers).forEach((key) => {
+    if (data === null && key.toLowerCase() === 'content-type') {
+      delete headers[key]
+    } else {
+      request.setRequestHeader(key, headers[key])
+    }
+  })
 
   request.send(data)
 }
