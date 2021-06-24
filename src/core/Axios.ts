485a113324b8bcadcb56995f937ddfcf439a8743
hui <125173679@qq.com>
2021-06-24 17:11:11 +0800

    feat: adding Function Overloading

--- src/core/Axios.ts
+++ src/core/Axios.ts
@@ -2,8 +2,16 @@ import { AxiosRequestConfig, AxiosPromise } from './../types/index'
 import dispatchRequest from './dispatchRequest'
 
 export default class Axios {
-  request(config: AxiosRequestConfig): AxiosPromise {
-    return dispatchRequest(config)
+  request(url: any, config?: AxiosRequestConfig): AxiosPromise {
+    if (typeof url === 'string') {
+      if (!config) {
+        config = {}
+      }
+      config.url = url
+    } else {
+      config = url
+    }
+    return dispatchRequest(config as AxiosRequestConfig)
   }
 
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
